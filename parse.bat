2107485ccb94741cc0bbc88d7d10b26ce3eeddf2
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-06-27 15:33:51 -0400

    Update parse.bat

--- parse.bat
+++ parse.bat
@@ -1,10 +1,25 @@
 @echo off
-set line=%1
-if not x%line:print=%==x%line% (
-set str=%line:print("=%
-set str=%str:")=%
-lib print %line%
-)
-if not x%line:input=%==x%line% (
-lib input %line%
-)
+
+set command=%1
+set args=%2
+
+echo %command%>nul
+call :case_%command% 2>nul || (
+    call :default_case
+ )
+
+:case_print
+  echo %args%
+  GOTO END_case
+:case_input
+  set /p input="%args%"
+  GOTO END_case
+:case_system
+  call %args%
+  GOTO END_case
+:default_case
+  ECHO Unknown Command: "%command%"
+  GOTO END_case
+:END_case
+  VER > NUL # reset ERRORLEVEL
+  GOTO :EOF # return from CALL
