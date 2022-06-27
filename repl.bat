a98c0eaa4862ebc8fc76b93a28800bd5b5fdc1ff
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-06-27 15:30:26 -0400

    Update repl.bat

--- repl.bat
+++ repl.bat
@@ -1,3 +1,11 @@
 @echo off
-set /p comand=">>> "
-parse comand
+for /F "delims=" %%a in ('date /t') do set date=%%a
+echo Baton 1.2 [%date:~0,-1%]
+echo Type "help" for help.
+:while
+(
+   set /p comand=">>> "
+	cd %~dp0
+	parse.bat %comand%
+   call :while
+)
