d24d61143422b4d1a30c7024cb556dab5d968aae
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-06-27 15:32:08 -0400

    Update handle.bat

--- Handle/handle.bat
+++ Handle/handle.bat
@@ -1,13 +1,17 @@
 @echo off
 set pname=%1
+rem set json="origin"
+rem set verEnpoint="https://pypi.org/pypi/%pname%/json"
+rem call winhttpjs.bat %verEndpoint% -method GET -saveTo CON | (set /p json= & set json)
+rem set version="origin"
+rem json %json% | (set /p version= & set version)
 rem Endpoint Format: https://pypi.io/packages/source/{ package_name_first_letter }/{ package_name }/{ package_name }-{ package_version }.tar.gz
-rem set endpoint="https://pypi.io/packages/source/%pname:~0,1%/%pname%/%pname%-%%.tar.gz"
-rem set endpoint="https://pypi.io/packages/source/p/ppci/ppci-0.5.4.tar.gz"
 set endpoint="https://pypi.io/packages/source/r/requests/requests-2.28.0.tar.gz"
-echo Handle Version 1.2
+rem set endpoint="https://pypi.io/packages/source/%pname:~0,1%/%pname%/%pname%-%version%.tar.gz"
+echo Handlebar Version 1.2
 echo.
 echo Downloading %pname% from PyPI API...
-call winhttpjs.bat %endpoint%  -method GET -saveTo response.tar.gz
+call winhttpjs.bat %endpoint% -method GET -saveTo response.tar.gz
 echo Download Finished: Starting Decompression...
 tar xzvf response.tar.gz -C ..\Installed
 echo Decompression Completed: Package Downloaded Successfully
