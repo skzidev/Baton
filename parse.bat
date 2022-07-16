b7fe1b7cbf49a5fa74e2fcedda5a41b46d0509e8
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-07-16 12:15:48 -0400

    Update parse.bat

--- parse.bat
+++ parse.bat
@@ -1,15 +1,24 @@
 @echo off
 
+if "%~1"=="" (
+  echo No Command Was Passed in to Parse
+  goto END_case
+)
+
 set command=%1
 set args=%2
 
-echo %command%>nul
 call :case_%command% 2>nul || (
     call :default_case
  )
 
 :case_print
-  echo %args%
+  if "%~1"=="" (
+    echo No Parameters Passed to Print.
+    goto END_case
+  ) else (
+    echo %args%
+  )
   GOTO END_case
 :case_input
   set /p input="%args%"
@@ -17,6 +26,8 @@ call :case_%command% 2>nul || (
 :case_system
   call %args%
   GOTO END_case
+:case_quit
+  exit
 :default_case
   ECHO Unknown Command: "%command%"
   GOTO END_case
