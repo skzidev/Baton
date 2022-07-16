a50a9b1021d8ebdfc35421a8e4a99cdd05131efb
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-07-16 12:16:24 -0400

    Update baton.bat

--- baton.bat
+++ baton.bat
@@ -4,5 +4,10 @@ repl
 )
 set fname=%1
 for /f "tokens=*" %%a in (%fname%) do (
-  parse %%a
+  for /f "tokens=2 delims=_(" %%b in ("%%a") do (
+    set arg=%%b
+  )
+  ::set arg=%arg:"=%
+  parse %%a %arg:~0,-1%
+  @echo off
 )
