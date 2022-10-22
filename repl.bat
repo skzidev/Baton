de44109f3d88ad380fa1d1f588e78bdea5fd6754
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-10-21 23:46:09 -0400

    Add Comments in REPL

--- repl.bat
+++ repl.bat
@@ -1,9 +1,12 @@
 @echo off
+rem Get the date and current directory.
 for /F "delims=" %%a in ('date /t') do set date=%%a
 for /F "delims=" %%b in ('cd') do set dir=%%b
 echo Baton 1.2 [%date:~0,-1%]
 echo Type "help" for help.
+rem This is how to write an infinite for loop in batch. (Using GOTO or CALL never works for some reason).
 for /L %%n IN () do (
+	rem Get the command, cd to a directory, run the code, cd back, and turn echo off (it was being turned back on).
 	set /p comand=">>> "
 	cd %~dp0
 	parse %comand%
