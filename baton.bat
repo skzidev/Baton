327380a87e21feb851da9c306649b952fca13852
anima.Ted() <89037615+AnimaTed6422@users.noreply.github.com>
2022-10-21 23:42:15 -0400

    Add Comments

--- baton.bat
+++ baton.bat
@@ -1,12 +1,18 @@
 @echo off
+rem Check if Arugments have been provided, If not, redirect the program to the REPL.
 if "%~1"=="" (
 repl
 )
+rem (The process will be terminated there so this code never runs and causes problems.)
 set fname=%1
+rem Get Filename and loop through each line in the file (this is an interpreter, after all).
 for /f "tokens=*" %%a in (%fname%) do (
   for /f "tokens=2 delims=_(" %%b in ("%%a") do (
     set arg=%%b
+    rem Get the arguments provided
   )
+  rem Parse the command by passing it in as well as the parameters.
   parse %%a %arg:~0,-1%
+  rem Echo was being turned on again, I have no idea why, but we have to turn echo off again.
   @echo off
 )
